8ee18b3d9fb810818b78d64fd0c3d0efbae04554
Camilaux <111092335+Camilaux@users.noreply.github.com>
2022-09-26 19:43:47 -0500

    Cambios

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,5 +1,5 @@
 class Zona:
-    def __init__(self, nombre=None, zoologico=None):
+    def __init__(self, nombre, zoologico):
         self._nombre = nombre
         self._zoo = zoologico
         self._animales = []
--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,5 +1,5 @@
 class Zoologico:
-    def __init__(self,nombre=None,ubicacion=None):
+    def __init__(self,nombre,ubicacion):
         self._nombre = nombre
         self._ubicacion = ubicacion
         self._zonas = []
